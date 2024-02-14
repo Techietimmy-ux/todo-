56298f7c848f7ead6961e602489387c5465c2a1e
techietimmy-ux <ikpediatimmy@gmail.com>
2024-02-15 00:34:36 +0100

    todo app

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
-const addTodo = document.querySelectorAll('form');
-const listItems = document.querySelector('.todos');
-
+const form = document.querySelectorAll('form')
+const listItems = document.querySelector('.todos')
 const searchTodo = document.querySelector('.search input')
 
 
 
-searchTodo.addEventListener('keyup', e => {
-    const newSearch = searchTodo.value.trim().toLowerCase();
 
-    Array.from(listItems.children).filter(child => {
-        return !child.textContent.toLowerCase().includes(newSearch)
 
-    }).map(child => {
-        child.classList.add('d-none')
-    })
 
-    Array.from(listItems.children).filter(child => {
-        return child.textContent.toLowerCase().includes(newSearch)
+const addTodo = (todo) => {
+    const newListItem = ` <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+</li>`
 
-    }).map(child => {
-        child.classList.remove('d-none')
-    })
+    listItems.innerHTML += newListItem
+}
+
+
+form[1].addEventListener('submit', e => {
+    e.preventDefault()
+
+    const todoValue = form[1].add.value.trim()
+    if (todoValue.length) {
+        addTodo(todoValue)
+    }
+    form[1].reset()
+
+
 
 
 })
 
 
-const htmlList = (todo) => {
-    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${todo}</span>
-    <i class="far fa-trash-alt delete"></i>
-</li>`
-    listItems.innerHTML += html
-
-}
-
-
-addTodo[1].addEventListener('submit', e => {
-    e.preventDefault()
-
-    const todoValue = addTodo[1].add.value.trim();
-    console.log(todoValue);
-    if (todoValue.length) {
-        htmlList(todoValue)
-    }
-    addTodo[1].reset()
-
-
-
-}
-)
-
+// deleting list items
 listItems.addEventListener('click', e => {
-    console.log(e.target.tagName);
 
     if (e.target.tagName === 'I') {
         e.target.parentElement.remove()
     }
+
+})
+
+// searching todo
+
+
+const filteredItems = (term) => {
+    Array.from(listItems.children).filter(item => {
+        return !item.textContent.toLowerCase().includes(term)
+    }).forEach(item => {
+        item.classList.add('d-none')
+    })
+
+    Array.from(listItems.children).filter(item => {
+        return item.textContent.toLowerCase().includes(term)
+    }).forEach(item => {
+        item.classList.remove('d-none')
+    })
+
+
+
+}
+
+
+
+searchTodo.addEventListener('keyup', e => {
+    const searchTodoItems = form[0].search.value.toLowerCase()
+
+    filteredItems(searchTodoItems)
 
 
 })
